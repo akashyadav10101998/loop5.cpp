e01f2d746efc6c7fac8cf142d08d757a32e49e29
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-09-05 17:19:18 +0530

    Add files via upload

---
+++ loop5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n;
+cout << "print all odd numbers from 1 to100";
+cin >> n;
+ int i = 1;
+while(i<=100)
+{
+cout <<i<<endl;
+i = i+2;
+}
+return 0;
+}
+
